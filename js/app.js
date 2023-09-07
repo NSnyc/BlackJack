b1e2fc9dd5498cd656444942317b641c1c637ccd
Enes <enesvelovic@gmail.com>
2023-09-07 13:17:16 -0400

    Fix Dealer Hidden Card When Dealer Hits

--- js/app.js
+++ js/app.js
@@ -48,6 +48,7 @@ let dealerSum = 0
 let message = ""
 let playerAceCount = 0
 let dealerAceCount = 0
+let isDealerTurn = false
 let messageContent = document.getElementById('message')
 let sumContent = document.getElementById('sum')
 let dealerContent = document.getElementById('dealerhand')
@@ -67,10 +68,11 @@ function init() {
   message = ""
   hitBtn.disabled = false
   stayBtn.disabled = false
+  isDealerTurn = false
   createDeck()
   dealCards()
-  showSums()
   render()
+  showSums()
   checkForWinner()
 }
 
@@ -107,6 +109,14 @@ function dealCards() {
   playerHand.push(deck.pop())
   dealerHand.push(deck.pop())
   dealerHand.push(deck.pop())
+  let cardImg = document.createElement("img");
+  cardImg.src = `../assets/images/backs/blue.svg`
+  cardImg.className = "hidden"
+  cardImg.dataset.card = dealerHand[0]
+  dealerContent.appendChild(cardImg)
+  let cardImg2 = document.createElement("img")
+  cardImg2.src = `../assets/images/cards/${dealerHand[1]}.svg`
+  dealerContent.appendChild(cardImg2)
 }
 
 function cardValue(card) {
@@ -147,15 +157,16 @@ function render() {
   dealerContent.innerHTML = ""
   playerContent.innerHTML = ""
   dealerHand.forEach((card, index) => {
-      let cardImg = document.createElement("img")
-      if (index === 0) {
-          cardImg.src = `../assets/images/backs/blue.svg`
-          cardImg.className = "hidden"
-          cardImg.dataset.card = card
-      } else {
-          cardImg.src = `../assets/images/cards/${card}.svg`
-      }
-      dealerContent.appendChild(cardImg)
+    let cardImg = document.createElement("img")
+    // If it's the first card of the dealer and it's not dealer's turn, render it as hidden
+    if (index === 0 && !isDealerTurn) {
+      cardImg.src = `../assets/images/backs/blue.svg`
+      cardImg.className = "hidden"
+      cardImg.dataset.card = card
+    } else {
+      cardImg.src = `../assets/images/cards/${card}.svg`
+    }
+    dealerContent.appendChild(cardImg)
   })
   playerHand.forEach(card => {
       let cardImg = document.createElement("img")
@@ -164,6 +175,7 @@ function render() {
   })
   showSums()
 }
+
 
 function checkForWinner() {
   if (dealerHand.length === 2 && sumHand(dealerHand) === 21) {
@@ -193,6 +205,12 @@ function checkForWinner() {
 function dealerTurn() {
   stayBtn.disabled = true
   hitBtn.disabled = true
+  isDealerTurn = true
+  let hiddenCard = dealerContent.querySelector('.hidden');
+  if (hiddenCard) {
+    hiddenCard.classList.remove('hidden');
+    hiddenCard.src = `../assets/images/cards/${hiddenCard.dataset.card}.svg`;
+  }
   if (sumHand(dealerHand) < 17) {
       dealerHand.push(deck.pop())
       render()
